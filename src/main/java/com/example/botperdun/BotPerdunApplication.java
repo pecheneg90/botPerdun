f7920170d796664b2f2a542d83b013b5e7fba127
pecheneg90 <manjavha@gmail.com>
2023-02-06 13:37:16 +0300

    Initial commit

---
+++ src/main/java/com/example/botperdun/BotPerdunApplication.java
@@ -0,0 +1,13 @@
+package com.example.botperdun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotPerdunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotPerdunApplication.class, args);
+	}
+
+}
